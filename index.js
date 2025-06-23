060f1c40a189679489e9950da92a0360c7fd1166
Najla Nadiva <nadiva979@gmail.com>
2025-06-23 18:18:05 +0700

    first commit

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
 
 // Middleware untuk parsing JSON
 app.use(express.json());
+app.use(cors());
 
 // Endpoint root
 app.get('/', (req, res) => {
